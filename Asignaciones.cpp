dc533146863b957f8d03fd3766e9c9385e44b1d6
ErickS2023 <141955911+ErickS2023@users.noreply.github.com>
2023-08-24 12:35:58 -0600

    actividad en clase

---
+++ Asignaciones.cpp
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+	
+	int a= 5;
+	int b= 6;
+
+    
+	
+	printf("La potencia de valor es %f\n",pow(5,6));
+    
+    printf("El modulo de valor es %d\n",a%b);
+    
+
+	printf("\a");//Alerta
+	return 0; 
+}
+
+
+
+
+
